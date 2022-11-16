e578989a55238c6d18234ecb74e0129b70effb61
Aryse Tansy <medictansy@gmail.com>
2022-11-16 16:34:19 -0500

    Auth: Login register routes

--- server/index.js
+++ server/index.js
@@ -22,6 +22,100 @@ app.get("/api/v1/test", (req, res) => {
 
 // UNIVERSAL ROUTE
 
+app.post("/api/v1/register", (req, res) => {
+  const { email, username, password } = req.body.user;
+
+  if (email.trim() === "" || username.trim() === "" || password.trim() === "") {
+    return res.status(422).json({ message: "All fields are required" });
+  } else if (password.length <= 5) {
+    return res
+      .status(400)
+      .json({ message: "Password must contain at least 6 characters" });
+  }
+
+  Example.findOne(
+    { $or: [{ username: username }, { email: email }] },
+    (err, doc) => {
+      if (err) {
+        return res.status(500).json({ message: "Oops, something went wrong" });
+      }
+
+      if (doc) {
+        return res
+          .status(409)
+          .json({ message: "Username or Email already registered" });
+      }
+
+      const hash = bcrypt.hashSync(password, 10);
+      const newUser = new Example({
+        email,
+        username,
+        password: hash,
+      });
+
+      newUser.save((err, doc) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "Oops, something went wrong, please try again." });
+        }
+
+        let token = jwt.sign({ username }, `${process.env.TOKEN_SECRET}`, {
+          expiresIn: "600s",
+        });
+
+        res.status(201).json({
+          username: doc.username,
+          token,
+        });
+      });
+    }
+  );
+});
+
+app.post("/api/v1/login", (req, res) => {
+  const { email, username, password } = req.body.user;
+  if (username.trim() === "" || password.trim() === "") {
+    return res.status(422).json({ message: "All fields are required" });
+  } else if (password.length <= 5) {
+    return res
+      .status(400)
+      .json({ message: "Password must contain at least 6 characters" });
+  }
+
+  Example.findOne({ username: username }, (err, doc) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "Oops, something went wrong, try again." });
+    }
+
+    if (!doc) {
+      return res.status(404).json({ message: "Username not registered" });
+    }
+
+    bcrypt.compare(password, doc.password, (err, match) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "Oops, something went wrong with your request" });
+      }
+      if (!match) {
+        return res.status(403).json({ message: "Incorrect password" });
+      }
+
+      let token = jwt.sign({ username }, `${process.env.TOKEN_SECRET}`, {
+        expiresIn: "600s",
+      });
+
+      res.status(201).json({
+        username: doc.username,
+        token,
+      });
+    });
+  });
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
