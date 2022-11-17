ab897e155a93647ddd7809bdf47111b9f72a7da4
Jackie O'Donnell <jackieodonnell@Jackies-MBP.attlocal.net>
2022-11-17 14:35:09 -0500

    return loans on user login

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,22 +1,49 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { restart } = require("nodemon");
+const { render } = require("..");
 const { pool } = require("../database/queries");
 
-const loginControl = (req, res) => {
-  const { username, password } = req.body;
+
+async function selectFrom(data, table, condition) {
+  try {
+    return await pool.query(`SELECT ${data} FROM ${table} ${condition}`)
+  } catch (err) {
+    return err.stack;
+  }
+}
+
+async function getUserByUsername(username){
+  return await selectFrom('*', 'users',`WHERE username = '${username}'`)
+}
+
+async function getLoansByUserId(userId){
+  return await selectFrom('*','loans',`WHERE lender_id = '${userId}' OR borrower_id = '${userId}'`);
+}
+
+
+const loginControl = async (req, res) => {  
+  const { username, password } = req.body;  
+  let results = await getUserByUsername(username);
+  const user = results.rows[0]
+  console.log(user);
+  results = await getLoansByUserId(user.user_id);
+  const loans = results.rows;
+  console.log(loans);
+
   if (username.trim() === "" || password.trim() === "")
     return res.status(422).json({ message: "All fields are required" });
 
-  pool.query(
-    `SELECT * FROM users WHERE username = '${username}'`,
-    (error, results) => {
-      if (error)
-        return res.status(500).json({ message: "Oops, something went wrong" });
+  // pool.query(
+  //   `SELECT * FROM users WHERE username = '${username}'`,
+  //   (error, results) => {
+  //     if (error)
+  //       return res.status(500).json({ message: "Oops, something went wrong" });
 
-      if (results.rows.length <= 0)
-        return res.status(404).json({ message: "User not registered" });
+  //     if (results.rows.length <= 0)
+  //       return res.status(404).json({ message: "User not registered" });
 
-      bcrypt.compare(password, results.rows[0].password, (err, match) => {
+    bcrypt.compare(password, user.password, (err, match) => {
         if (err) {
           return res
             .status(500)
@@ -30,13 +57,18 @@ const loginControl = (req, res) => {
           expiresIn: "600s",
         });
 
-        res.status(201).json({
-          user: results.rows[0],
+        return res.status(201).json({
+          user,
           token,
+          loans,
         });
       });
-    }
-  );
-};
+
+  // console.log(user.user_id);
+  // pool.query(`SELECT * FROM loans WHERE lender_id = '${user.user_id}' OR borrower_id = '${borrower_id}';`, (error, results) => {
+  //   console.log(results);
+  //   res.send(results);
+    };
+
 
 module.exports = { loginControl };
