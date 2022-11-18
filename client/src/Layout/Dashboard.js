588c877bf28c6ad9985b047448bdf657135e57d2
Denasia <dfurniss@ymail.com>
2022-11-18 15:41:07 -0500

    comments on where button should go once those pages are made

--- client/src/Layout/Dashboard.js
+++ client/src/Layout/Dashboard.js
@@ -3,39 +3,35 @@ import { useNavigate } from "react-router-dom";
 
 export default function Dashboard({ token, user, loans}){
     const navigate = useNavigate();
-    console.log(loans)
 
     return (
         <div>
-            <h1>Hello, {user}!</h1>
-            {/* all of the users entries rendered */}
-            {/* <h2>Log</h2> */}
+            <h1>Hello, {user.username}!</h1>
+            <h2>Activity</h2>
             {loans.length && loans.map((loan)=>{
                 return (
-                    <div>
-                        {/* idea 1: as a card  */}
+                    <div key={loan.id}>
+                    {/* idea 1: as a card  */}
                         {/* <p>lender: {loan.lender === user ? "you" : loan.lender}</p>
                         <p>borrower: {loan.borrower === user ? "you" : loan.borrower}</p>
                         <p>{loan.due_date}</p>
                         <p>{loan.description}</p> */}
 
-                        {/* idea 2: as a sentence  */}
+                    {/* idea 2: as a sentence  */}
                         <p>{loan.borrower === user ? "you" : loan.borrower} borrowed ${loan.amount} from {loan.lender === user ? "you" : loan.lender} for {loan.description}</p>
                     </div>
                 )
             })}
 
-            {/* <button
+        {/* this button will be rendered once another user is searched for
+             <button
                 onClick={()=>{
                 navigate("/create")
                 }
                 }>New Entry</button> */}
-
-            {/* this button will end up on the user's "Accout" page; an option to log out */}
-            {/* <button
-                onClick={()=>{
-                    localStorage.removeItem("userValidation");
-                }}>Log out</button> */}
+        {/* this button will be rendered on the user's account page
+            <button
+                onClick={()=>navigate(`/account/${user.username}`)}>view account</button> */}
         </div>
     )
 };
