d983b57da6c742e808d365a13649b84f02b115f4
LaToya Simon <1stforgottentoy@gmail.com>
2022-11-18 11:48:58 -0800

    search functionality that needs to be tweaked

--- server/database/queries.js
+++ server/database/queries.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "payback_app",
-  password: "postgres1",
+  user: "znjfdvao",
+  host: "peanut.db.elephantsql.com",
+  database: "znjfdvao",
+  password: "7nl8WuulhBMOETrjqqn30WQsnzxdFXCe",
   port: 5432,
 });
+
 const getUsers = (request, response) => {
   console.log("getUsers route hit");
   pool.query("SELECT * FROM users ORDER BY user_id ASC", (error, results) => {
@@ -17,7 +18,19 @@ const getUsers = (request, response) => {
   });
 };
 
+const getUserName = (request, response) => {
+  console.log("getUsers route hit");
+  pool.query("SELECT username FROM users ORDER BY email ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).json(results.rows);
+    console.log(results);
+  });
+};
+
 module.exports = {
   getUsers,
+  getUserName,
   pool,
 };
